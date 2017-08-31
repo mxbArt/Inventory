83a98eb88eda79bf6bc409292692c2cfbabdcbd6
Ivan Antonov <ivan_antonov@live.ru>
2017-08-31 23:30:44 +0300

    deleted storekeeper role

--- Frontend/InventoryApp/src/app/fake-data/fake-auth-data.ts
+++ Frontend/InventoryApp/src/app/fake-data/fake-auth-data.ts
@@ -10,12 +10,6 @@ let fakeUsers: Array<IUser> = [
     role: Roles.driver
   },
   {
-    id: 'd70f5120-8366-40b5-86b6-e587b1bb784a',
-    email: 'storekeeper@gmail.com',
-    password: 'Storekeeper123',
-    role: Roles.storekeeper
-  },
-  {
     id: '96b93c5d-7841-4a59-b09b-2a4e48e4778a',
     email: 'admin@gmail.com',
     password: 'Admin123',
