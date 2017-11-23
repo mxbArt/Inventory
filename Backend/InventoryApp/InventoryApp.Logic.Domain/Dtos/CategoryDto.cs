6e1b041c645496e5086565e1065c574b1cc315ec
mxbArt <berezovskiy.96@bk.ru>
2017-11-23 19:25:13 +0300

    Dto models were created.

---
+++ Backend/InventoryApp/InventoryApp.Logic.Domain/Dtos/CategoryDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InventoryApp.Logic.Domain.Dtos
+{
+    public class CategoryDto
+    {
+        public CategoryDto()
+        {
+            Products = new List<ProductDto>();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ImgPath { get; set; }
+
+        public List<ProductDto> Products { get; set; }
+    }
+}
---
+++ Backend/InventoryApp/InventoryApp.Logic.Domain/Dtos/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InventoryApp.Logic.Domain.Dtos
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string Description { get; set; }
+        public string Measurment { get; set; }
+        public string ImgPath { get; set; }
+
+        public CategoryDto Category { get; set; }
+    }
+}
